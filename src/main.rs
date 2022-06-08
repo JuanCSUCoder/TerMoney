035840c45be412e1a14483a41eea319cc516e4aa
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2022-06-08 18:31:09 -0500

    Translate variable names to english

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ mod question;
 use crate::question::Question;
 
 fn main() {
-  println!();
-	let origen = Question::new("Origin Account: ").not_null().not_containing(" ").ask();
+	let origin = Question::new("Origin Account: ").not_null().not_containing(" ").ask();
 
-	let destino = Question::new("Destination Account: ").not_null().not_containing(" ").ask();
+	let dest = Question::new("Destination Account: ").not_null().not_containing(" ").ask();
 
-	let cantidad = Question::new("Amount: ").not_null().not_containing(".").not_containing(",").not_containing("$").ask_number();
+	let amount = Question::new("Amount: ").not_null().not_containing(".").not_containing(",").not_containing("$").ask_number();
 
-	println!("${} will be sent from {} to {}.", cantidad, origen, destino);
+	println!("${} will be sent from {} to {}.", amount, origin, dest);
 
 	match Question::new("Want to proceed? ").not_null().ask_yn() {
 		true => println!("YES!"),
