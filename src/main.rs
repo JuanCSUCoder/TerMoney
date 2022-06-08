d6d969c61516622277e995e2439ff4830729e90c
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2022-06-08 14:14:19 -0500

    Implement Question class

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use crate::question::Question;
 
 fn main() {
   println!();
-	let origen = Question::new("Escriba la cuenta de origen: ").not_null().ask();
+	let origen = Question::new("Escriba la cuenta de origen: ").not_null().not_containing(" ").ask();
 
-	let destino = Question::new("Escriba la cuenta de destino: ").not_null().ask();
+	let destino = Question::new("Escriba la cuenta de destino: ").not_null().not_containing(" ").ask();
 
 	println!("Se va a enviar $10000 desde {} hasta {}.", origen, destino);
 
