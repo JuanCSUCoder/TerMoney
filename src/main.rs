d5dc968751a1d574c9142f7076c25c52b55e698c
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2022-06-13 12:46:36 -0500

    Implement serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 mod question;
 
+use std::{fs::File, io::Read};
+
+use serde_json::Value;
+
 use crate::question::Question;
 
 fn main() {
+	// Open transactions file
+	let mut f = match File::open("registry.json") {
+		Ok(file) => file,
+		Err(_) => File::create("registry.json").expect("Unable to open the registry of transactions")
+	};
+
+	let mut f_contents = String::new();
+	f.read_to_string(&mut f_contents).expect("Unable to read the registry of transactions");
+
+	let json: Value = serde_json::from_str(&f_contents).expect("Transaction Registry Corrupted");
+
 	let origin = Question::new("Origin Account: ").not_null().not_containing(" ").ask();
 
 	let dest = Question::new("Destination Account: ").not_null().not_containing(" ").ask();
