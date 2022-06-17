97ded741c756df0b1db3078c419fb5555aeb5c94
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2022-06-17 11:24:15 -0500

    Completed transaction struct and constructor

--- src/registry/transaction/mod.rs
+++ src/registry/transaction/mod.rs
@@ -1,5 +1,6 @@
 mod money;
 
+use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 
 use self::money::Money;
@@ -7,22 +8,34 @@ use self::money::Money;
 #[derive(Serialize, Deserialize)]
 /// A transaction
 pub struct Transaction {
+	id: i64,
+	continues: Option<i64>,
+
+	time: DateTime<Utc>,
+
 	from: String,
 	to: String,
 	description: Option<String>,
+
 	money: Money
 }
 
 impl Transaction {
 	/// Create a new Transaction
-	pub fn new(from: String, to: String, amount: i64, exponent: i64) -> Self {
+	pub fn new(id: i64, continues: Option<i64>, from: String, to: String, amount: i64, exponent: i64, description: Option<String>) -> Self {
 		let money = Money::new(amount, exponent);
 
 		Self {
+			id,
+			continues,
+
+			time: Utc::now(),
+
 			from,
 			to,
-			money,
-			description: None
+			description,
+
+			money
 		}
 	}
 }
