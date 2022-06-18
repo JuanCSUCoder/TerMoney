9043da8bbdd4f55539ecae69f877cdbec9b7ef87
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2022-06-17 19:03:11 -0500

    Implemented add_from_cli in the registry

--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -3,6 +3,7 @@ mod transaction;
 use std::{fs::File, io::{Read, Write}};
 
 use self::transaction::Transaction;
+use crate::question::Question;
 
 /// A transaction registry
 pub struct Registry {
@@ -61,11 +62,33 @@ impl Registry {
 		}
 	}
 
+	/// Adds a new transaction promting the user through the CLI
+	pub fn add_from_cli(&mut self) {
+		let from = Question::new("Origin Account: ").not_null().not_containing(" ").ask();
+
+		let to = Question::new("Destination Account: ").not_null().not_containing(" ").ask();
+
+		let amount = Question::new("Amount: ").not_null().not_containing(".").not_containing(",").not_containing("$").ask_number();
+
+		println!("${} will be sent from {} to {}.", amount, from, to);
+
+		let new_transact = Transaction::new(self.generate_id(), None, from, to, amount, 0, None);
+
+		self.transactions.push(new_transact);
+	}
+
 	/// Saves the current state to the registry file
 	pub fn save(&self) {
 			let mut f = File::create(&self.file_path).expect("Unable to rewrite the file");
 			let json = serde_json::to_string(&self.transactions).expect("Error serializing");
 
 			f.write_all(json.as_bytes()).expect("Unable to rewrite the file");
+	}
+
+	/// Generates a new ID an updates the ID count
+	fn generate_id(&mut self) -> i64{
+		self.new_id += 1;
+
+		self.new_id-1
 	}
 }
